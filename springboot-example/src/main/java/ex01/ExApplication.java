f079dc9f7aded988fa535367bddfc744e4bf06b6
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-01-20 16:54:54 +0900

    springboot-example - feat: 빈 등록

--- springboot-example/src/main/java/ex01/ExApplication.java
+++ springboot-example/src/main/java/ex01/ExApplication.java
@@ -1,14 +1,23 @@
 package ex01;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ExApplication {
+public class ExApplication { // Bootstrapping Class
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExApplication.class, args);
-
+	@Bean
+	public Map<?, ?> myMap() {
+		return Map.of("greetings", "hello world");
 	}
 
+	public static void main(String[] args) {
+		try(ConfigurableApplicationContext ac = SpringApplication.run(ExApplication.class, args);){
+			System.out.println("ExApplication runs.. " + ac.getBean("myMap", Map.class).get("greetings"));
+		}
+	}
 }
