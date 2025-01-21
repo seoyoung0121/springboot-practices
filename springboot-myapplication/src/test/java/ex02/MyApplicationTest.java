dc1d5710deddff2ba43ab8c59a0685702018f358
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-01-21 11:15:09 +0900

    springboot-myapplication - feat: @SpringBootApplication & @SpringBootTest

---
+++ springboot-myapplication/src/test/java/ex02/MyApplicationTest.java
@@ -0,0 +1,24 @@
+package ex02;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import ex02.component.MyComponent;
+
+@SpringBootTest
+public class MyApplicationTest {
+	@Autowired
+	private ApplicationContext applicationContext;
+	
+	@Test
+	public void testMyComponentNotNull() {
+		MyComponent myComponent = applicationContext.getBean("MyComponent", MyComponent.class);
+		assertNotNull(myComponent);
+	}
+}
